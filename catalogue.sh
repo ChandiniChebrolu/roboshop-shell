c495100c8ab2d4b8f94e20db9edf860470cb9b62
chandini <chebroluchandini123@gmail.com>
2023-08-25 12:08:05 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -51,6 +51,6 @@ validate $? "copying mongo.repo"
 
 yum install mongodb-org-shell -y &>> $Logfile
 validate $? "installing mongo client"
-mongo --host 34.226.222.125 </app/schema/catalogue.js &>> $Logfile
+mongo --host 54.81.178.33 </app/schema/catalogue.js &>> $Logfile
 validate $? "loading catalogue data in to mongodb"
 
